e3ea282be55264eae1cd398c590146036f23f646
hanum <rntls123@naver.com>
2021-08-30 23:56:05 +0900

    08.30 23:55 Practice component props, dynamic component generation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+
+// component 는 대문자로 시작해야함. 규칙
+
+const Test = ({ name, age }) => {
+  return (
+    <div>
+      <h1> {name}  </h1>
+      <span> {age} </span>
+    </div>
+  );
+};
+
+const list = [
+  { "name": "a", "age": 20 },
+  { "name": "b", "age": 26 },
+  { "name": "c", "age": 22 },
+]
 
 function App() {
   return (
     <div className="App">
-      HELLO!
+      {list.map(element => <Test name={element.name} age={element.age} />)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+/*
+ 1. jsx => 위와 같이 js 와 react 를 혼합해 사용하는 react 내의 개념
+ App 과 같은 형태를 react 에서 component 라 칭함.
+ 즉, jsx 는 js 만의 HTML, component 를 만들고 어떻게 사용하는지에 대한 것.
+
+ 2. jsx 에서 component 는 정보를 담아 보낼 수 있다.
+ */
+
 
