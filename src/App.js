806675e36d0aaaf66d036e91f728465040088858
hanum <rntls123@naver.com>
2021-08-31 00:33:22 +0900

    08.31 00:32 Check component using prop-types

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // component 는 대문자로 시작해야함. 규칙
-
 const Test = ({ name, age }) => {
   return (
     <div>
       <h1> {name}  </h1>
-      <span> {age} </span>
+      <h3> {age} </h3>
     </div>
   );
 };
 
+Test.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+};
+
 const list = [
-  { "name": "a", "age": 20 },
-  { "name": "b", "age": 26 },
-  { "name": "c", "age": 22 },
+  { id: 1, "name": "a", "age": 20 },
+  { id: 2, "name": "b", "age": 26 },
+  { id: 3, "name": "c", "age": 22 },
 ]
 
 function App() {
   return (
     <div className="App">
-      {list.map(element => <Test name={element.name} age={element.age} />)}
+      {list.map(element => <Test key={element.id} name={element.name} age={element.age} />)}
     </div>
   );
-}
+};
+/* react 내에서 모든 element 는 달라야함. 이를 구분하기 위해 key 값을 넣어줌. */
 
 export default App;
