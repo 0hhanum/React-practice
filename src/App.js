a2ce2e9d4f39823000d62e2bc4a10698673ebce7
hanum <rntls123@naver.com>
2021-09-01 01:01:47 +0900

    09.01 01:00 about component function order

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ Test.propTypes = {
   age: PropTypes.number.isRequired,
 };
 
-const list = [
-  { id: 1, "name": "a", "age": 20 },
-  { id: 2, "name": "b", "age": 26 },
-  { id: 3, "name": "c", "age": 22 },
-]
+// const list = [
+//   { id: 1, "name": "a", "age": 20 },
+//   { id: 2, "name": "b", "age": 26 },
+//   { id: 3, "name": "c", "age": 22 },
+// ]
 
 // function component
 // function App() {
@@ -34,8 +34,9 @@ const list = [
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
   };
+  /*
   add = () => {
     // this.setState({ count: this.state.count + 1 })
     this.setState(current => ({ count: current.count + 1 }))
@@ -45,16 +46,16 @@ class App extends React.Component {
     // this.setState({ count: this.state.count - 1 })
     this.setState(current => ({ count: current.count - 1 }))
   };
+  */
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 600);
+  };
   render() {
-    return (
-      <div>
-        <h1>
-          Class Component {this.state.count}
-        </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+
   };
 };
 
