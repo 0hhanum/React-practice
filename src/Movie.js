c00979c4391dce625171b274f1f0a070efaea231
hanum <rntls123@naver.com>
2021-09-01 22:42:10 +0900

    09.01 22:42 Deploy

--- src/Movie.js
+++ src/Movie.js
@@ -7,7 +7,7 @@ function Movie({ year, title, summary, poster, genres }) {
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h3 className="movie__year">{year}</h3>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
             <ul className="movie__genres">
                 {genres.map((genre, index) =>
                     <li key={index} className="genres__genre">  {genre} </li>
