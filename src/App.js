801cb9ecf43f3634d4c29b2aab379ee4870ba384
hanum <rntls123@naver.com>
2021-09-01 22:09:14 +0900

    09.01 22:08 Add genres & create CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
+
 // component 는 대문자로 시작해야함. 규칙
 // const Test = ({ name, age }) => {
 //   return (
@@ -58,9 +60,31 @@ class App extends React.Component {
   };
   render() {
     const { movies, isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie => (
-      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-    ))} </div>;
+    return (
+      // class 로 하면 리액트가 js 의 Class 와 혼동하니 className 부여
+      <section className="container">
+        {isLoading
+          ? <div className="loader">
+            <span className="loader__text">Loading..</span>
+          </div>
+          : (
+            <div className="movies">
+              {
+                movies.map(movie => (
+                  < Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres} />
+                ))}
+            </div>
+          )
+        }
+      </section>
+    );
   };
 };
 
